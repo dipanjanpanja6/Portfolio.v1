43d5ba893b38afbaa38674753b6f7c8a7277025c
Dipanjan Panja <dipanjanpanja6@gmail.com>
2020-10-22 18:50:15 +0530

    init

---
+++ lib/constants/strings.dart
@@ -0,0 +1,95 @@
+import 'package:portfolio/models/education.dart';
+
+class Strings {
+  Strings._();
+
+  // home:----------------------------------------------------------------------
+  static const String portfolio = 'dp';
+  static const String this_web = 'Yes this website uses f***ing ';
+  static const String cookies = 'cookies';
+  static const String in_flutter = 'in flutter';
+  static const String heart = ' ❤ ';
+  static const String made_with = 'Made with';
+
+  // intro:---------------------------------------------------------------------
+  static const String hello = 'Hello';
+  static const String i_am_zubair = '\nI am\nDipanjan';
+  static const String design = 'DES_\nIGN';
+  static const String mobile_dev =
+      'Full stack Developer\nbased in Kolkata,India.❤';
+
+  // about:-----------------------------------------------------------------------
+  static const String about_me = 'About Me';
+  static const String about = 'Ab_\nout';
+  static const String me = ' Me';
+  static const String headline =
+      'I\'am Dipanjan Panja, Full stack web Developer from India';
+  static const String summary =
+      'A kid who was once given a mouse, a keyboard & a screen. Ever since then he writes in a language that few comprehend. Not looking for a tech giant to fill my pocket with money but a problem that needs to be solved by collaborating. ';
+  // 'A 4th year Computer Science & Engineering undergraduate student with an interest in Dart,Node js,React JS and beginner in AI looking for an opportunity to utilize my skill in professional work experience. To perform well in a challenging environment that offer scope for personal and professional growth and in the process. Specialize in thinking outside the box to find unique solutions to difficult engineering problems.';
+  // 'Focused professional having excellent technical and communication skills, and offering 6 years of experience in Computer industry. Proficient at designing and formulating test automation frameworks, writing code in various languages, feature development and implementation. Specialize in thinking outside the box to find unique solutions to difficult engineering problems.';
+
+  // skills:--------------------------------------------------------------------
+  static const String skills = 'Sk_\nills';
+  static const String experience = 'Experience';
+  static const String skills_i_have = '\nWhat Skills I Have';
+  static const String rights_reserved =
+      '© 2019 IOTECK SOLUTIONS. ALL RIGHTS RESERVED.';
+
+  static final skillsList = [
+    'JS',
+    'JSX'
+        'Java',
+    'Vue JS',
+    'Dart',
+    'Flutter',
+    'Android',
+    'iOS',
+    'React',
+    'React Native',
+    'AWS',
+    'NginX',
+    'Figma',
+    'Firebase',
+    'Anguler JS',
+  ];
+
+  static final educationList = [
+    Education(
+      'July 2020',
+      'Present',
+      'RoomLelo',
+      'Software Engineer',
+    ),
+    Education(
+      'May 2020',
+      'Present',
+      'QrioctyBox',
+      'Sr. Software Engineer',
+    ),
+  ];
+
+  // hire:----------------------------------------------------------------------
+  static const String hire = 'HIRE';
+  static const String always_interested =
+      'I\'m always interested about cool stuff. Are you minding a project?\n';
+  static const String lets_talk = 'Let\'s talk.';
+
+  // menu items:----------------------------------------------------------------
+  static const String menu_facebook = 'FACEBOOK';
+  static const String menu_twitter = 'TWITTER';
+  static const String menu_linked_in = 'LINKEDIN';
+  static const String menu_medium = ' MEDIUM ';
+  static const String menu_github = 'GITHUB';
+  static const String menu_mail = 'MAIL';
+
+  // links:-----------------------------------------------------------------------
+  static const String menu_facebook_link =
+      'https://www.facebook.com/dipanjanpanja6';
+  static const String menu_twitter_link = 'https://twitter.com/dipanjanpanja6';
+  static const String menu_linked_in_link =
+      'https://www.linkedin.com/in/dipanjanpanja6/';
+  static const String menu_medium_link = 'https://medium.com/@dipanjanpanja6';
+  static const String menu_github_link = 'https://github.com/dipanjanpanja6';
+  static const String menu_mail_link = 'mailto:dipanjanpanja6@gmail.com';
+}
